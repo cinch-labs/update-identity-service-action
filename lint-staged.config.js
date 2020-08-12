298db887dec6aeaf3951cbebe3c965a6c7183b88
Bertie Blackman <blackmanrgh@gmail.com>
2020-08-12 10:48:16 +0100

    Updates pipeline config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.ts': ['eslint --fix'],
+  '*.ts': ['eslint --fix', 'npm run build && npm run package'],
   '**/*.ts': () => 'tsc -p tsconfig.json --noEmit',
   '*.{ts,json,md,yml}': ['prettier --write'],
 }
